0e7454d4317d6085928babe91fd78e56a3426eac
THEBOIII <juneppark44@gmail.com>
2026-05-10 18:54:30 +0200

    Add Truck data class with properties

--- Data/model /Truck.kt
+++ Data/model /Truck.kt
@@ -1,1 +1,9 @@
+package com.garage.app.data.model
 
+data class Truck(
+    val id: Int,
+    val truckNumber: String,
+    val kilometers: Int,
+    val condition: String
+)
+// THIS IS JUST A CODE SNIPPET TO HELP YOU BEGIN
